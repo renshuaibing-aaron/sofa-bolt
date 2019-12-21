92669acabecb8db9ef9a121e930ed5d38d2bce9f
renshuaibing <renshuaibing@hikvision.com.cn>
2019-12-22 00:23:47 +0800

    sofa-bolt 源码写

--- src/main/java/com/alipay/remoting/aaron/fullduplex/connection/MyConnectEventProcessor.java
+++ src/main/java/com/alipay/remoting/aaron/fullduplex/connection/MyConnectEventProcessor.java
@@ -3,7 +3,7 @@ package com.alipay.remoting.aaron.fullduplex.connection;
 import com.alipay.remoting.Connection;
 import com.alipay.remoting.ConnectionEventProcessor;
 
-public class MyCONNECTEventProcessor implements ConnectionEventProcessor {
+public class MyConnectEventProcessor implements ConnectionEventProcessor {
     // 存储连接，用于服务端向客户端发起远程通信
     private Connection connection;
 
--- src/main/java/com/alipay/remoting/aaron/fullduplex/connection/MyServer.java
+++ src/main/java/com/alipay/remoting/aaron/fullduplex/connection/MyServer.java
@@ -9,19 +9,28 @@ import com.alipay.remoting.rpc.RpcServer;
 
 public class MyServer {
     public static void main(String[] args) throws RemotingException, InterruptedException {
+
         RpcServer server= new RpcServer(8888);
+
         MyServerUserProcessor serverUserProcessor = new MyServerUserProcessor();
+
         server.registerUserProcessor(serverUserProcessor);
+
         // 创建并注册 ConnectionEventType.CONNECT 连接事件处理器
-        MyCONNECTEventProcessor connectEventProcessor = new MyCONNECTEventProcessor();
+        MyConnectEventProcessor connectEventProcessor = new MyConnectEventProcessor();
         server.addConnectionEventProcessor(ConnectionEventType.CONNECT, connectEventProcessor);
+
+
         if (server.start()) {
+
             System.out.println("server start success!");
             // 模拟去其他事情
             Thread.sleep(10000);
             System.out.println("======服务器主动向客户端发消息=========="+connectEventProcessor.getConnection());
             MyRequest request = new MyRequest();
             request.setReq("hi, bolt-client,我是服务端");
+
+
             // 向 connectEventProcessor 存储的 connection 发起请求
             MyResponse resp = (MyResponse)server.invokeSync(connectEventProcessor.getConnection(), request, 10000);
 
